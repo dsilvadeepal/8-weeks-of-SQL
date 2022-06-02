e5c1b8eee9cb409c7784f91cb0373957264ba8a6
Deepal DSilva <dsilvadeepal@gmail.com>
2022-06-02 13:37:55 -0400

    Update 1-dannys-diner.sql

--- #1 - Danny's Diner/1-dannys-diner.sql
+++ #1 - Danny's Diner/1-dannys-diner.sql
@@ -2,8 +2,17 @@
 
 SELECT
   	a.customer_id, 
-    sum(b.price)
+    sum(b.price) as Spent
 FROM dannys_diner.sales a
 LEFT JOIN dannys_diner.menu b
 ON a.product_id = b.product_id
 GROUP BY a.customer_id;
+
+-- 2. How many days has each customer visited the restaurant?
+
+SELECT
+  	customer_id, 
+    count(distinct order_date) as Days
+FROM dannys_diner.sales
+GROUP BY customer_id; 
+
